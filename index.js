870fce599b2b1562eb6124d0accd5f5e4c515b48
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 15:15:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+  
+  speak(name) {
+    return `{$this.name} says meow!`
+  }
+}
+ 
+class Dog {
+ 
+}
+ 
+class Bird {
+  
+}
